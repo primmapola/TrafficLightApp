46df413a8688eb7c616b1fb771ee791fc37f1a79
don grigory <dingrygory29@gmail.com>
2022-12-10 22:11:16 +0300

    Initial Commit

---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Grigory Don on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
