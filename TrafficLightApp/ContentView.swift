7b766445cb429834151907b33ffb4236ae75a210
don grigory <dingrygory29@gmail.com>
2022-12-10 22:54:38 +0300

    final

--- TrafficLightApp/ContentView.swift
+++ TrafficLightApp/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var opacityAttributeRed = 0.3
+    @State private var opacityAttributeYellow = 0.3
+    @State private var opacityAttributeGreen = 0.3
+    @State private var buttonName = "START"
+    @State private var tempLigth: Color = .red
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle()
+                .frame(width: 140, height: 140)
+                .foregroundColor(.red)
+                .opacity(opacityAttributeRed)
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 3)
+                )
+            Circle()
+                .frame(width: 140, height: 140)
+                .foregroundColor(.yellow)
+                .opacity(opacityAttributeYellow)
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 3)
+                )
+            Circle()
+                .frame(width: 140, height: 140)
+                .foregroundColor(.green)
+                .opacity(opacityAttributeGreen)
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 3)
+                )
+            Spacer()
+            Button(action: buttonPressed){
+                Text(buttonName)
+                    .font(.title)
+            }
         }
         .padding()
+    }
+    
+    private func buttonPressed() {
+        buttonName = "NEXT"
+        switch tempLigth {
+        case .red:
+            opacityAttributeRed = 1
+            opacityAttributeGreen = 0.3
+            tempLigth = .yellow
+        case .yellow:
+            opacityAttributeYellow = 1
+            opacityAttributeRed = 0.3
+            tempLigth = .green
+        default:
+            opacityAttributeGreen = 1
+            opacityAttributeYellow = 0.3
+            tempLigth = .red
+        }
     }
 }
 
